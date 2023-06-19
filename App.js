1cd8fac28e8c5df795969478bba1aa3e4cc453cb
ADS 2021.2N <36124872021.2N@senacrjedu.local>
2023-06-19 19:36:36 -0300

    Alteração do código para salvar os ceps na tela do dispositivo

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { Provider as PaperProvider, TextInput, Button, Snackbar } from 'react-native-paper';
 
-export default function App() {
+const App = () => {
+  const [cep, setCep] = useState('');
+  const [addressData, setAddressData] = useState({});
+  const [errorSnackbarVisible, setErrorSnackbarVisible] = useState(false);
+  const [searchedCeps, setSearchedCeps] = useState([]);
+
+  const fetchAddressData = async () => {
+    try {
+      const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      const data = await response.json();
+
+      console.log(data);
+      if (response.ok) {
+        setAddressData(data);
+        setSearchedCeps(prevCeps => [...prevCeps, cep]);
+      } else {
+        setErrorSnackbarVisible(true);
+      }
+    } catch (error) {
+      console.error(error);
+      setErrorSnackbarVisible(true);
+    }
+  };
+
+  const dismissErrorSnackbar = () => {
+    setErrorSnackbarVisible(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <ScrollView contentContainerStyle={styles.container}>
+        <TextInput
+          label="CEP"
+          value={cep}
+          onChangeText={text => setCep(text)}
+          style={styles.input}
+        />
+        <Button mode="contained" onPress={fetchAddressData}>
+          Buscar Endereço
+        </Button>
+
+        {Object.keys(addressData).map(key => (
+          <TextInput
+            key={key}
+            label={key.toUpperCase()}
+            value={addressData[key] || ''}
+            disabled
+            style={styles.input}
+          />
+        ))}
+
+        <Text style={styles.cepList}>
+          CEPs pesquisados: {searchedCeps.join(", ")}
+        </Text>
+
+        <Snackbar
+          visible={errorSnackbarVisible}
+          onDismiss={dismissErrorSnackbar}
+          duration={3000}
+        >
+          Ocorreu um erro ao buscar o endereço. Verifique o CEP e tente novamente.
+        </Snackbar>
+      </ScrollView>
+    </PaperProvider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexGrow: 1,
+    padding: 16,
     justifyContent: 'center',
   },
+  input: {
+    marginBottom: 12,
+  },
+  cepList: {
+    fontSize: 16,
+    marginBottom: 12,
+    fontWeight: 'bold',
+  },
 });
+
+export default App;
